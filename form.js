1197704ff23aefb7d726b79c00fd990f0059cb1b
Abubakar9894 <abubakarsaddique786.tsk@gmail.com>
2023-08-16 18:01:15 +0500

    second commit

--- form.js
+++ form.js
@@ -2,50 +2,69 @@ document.addEventListener('DOMContentLoaded', () => {
     const signupForm = document.getElementById('signup-form');
     const loginForm = document.getElementById('login-form');
 
-    // ... Signup code ...
     signupForm.addEventListener('submit', async (e) => {
       e.preventDefault();
+      
+      const signupUsername = document.getElementById('signup-username').value;
+      const signupEmail = document.getElementById('signup-email').value;
+      const signupPassword = document.getElementById('signup-password').value;
+      const signupConfirmPassword = document.getElementById('signup-confirm-password').value;
+      
+      if (signupPassword.length < 6) {
+        alert('Password should be at least 6 characters long.');
+        return;
+      }
+      
+      if (signupPassword !== signupConfirmPassword) {
+        alert('Passwords do not match.');
+        return;
+      }
 
-      const username = document.getElementById('signup-username').value;
-const email = document.getElementById('signup-email').value;
-const phone = document.getElementById('signup-phone').value;
-const password = document.getElementById('signup-password').value;
-const age = document.getElementById('signup-age').value;
-const address = document.getElementById('signup-address').value;
-
-const newUser = {
-  username,
-  email,
-  phone,
-  password,
-  age,
-  address,
-};
-
-try {
-  const response = await fetch('http://localhost:3000/users', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(newUser),
-  });
-
-  if (response.ok) {
-    alert('User registered successfully!');
-    signupForm.reset();
-  } else {
-    alert('An error occurred. Please try again.');
-  }
-} catch (error) {
-  console.error('Error:', error);
-}
-
+      try {
+        const usersResponse = await fetch('http://localhost:3000/users');
+        const users = await usersResponse.json();
+        
+        const existingUserByUsername = users.find(user => user.username === signupUsername);
+        if (existingUserByUsername) {
+          alert('Username already exists. Please choose a different username.');
+          return;
+        }
+        
+        const existingUserByEmail = users.find(user => user.email === signupEmail);
+        if (existingUserByEmail) {
+          alert('Email already exists. Please use a different email.');
+          return;
+        }
+        
+        const newUser = {
+          username: signupUsername,
+          email: signupEmail,
+          password: signupPassword,
+        };
+        
+        const response = await fetch('http://localhost:3000/users', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newUser),
+        });
+      
+        if (response.ok) {
+          alert('User registered successfully!');
+          signupForm.reset();
+        } else {
+          alert('An error occurred. Please try again.');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
     });
+
     //login code
     loginForm.addEventListener('submit', async (e) => {
       e.preventDefault();
-      
+
       const loginCredential = document.getElementById('login-credential').value;
 const loginPassword = document.getElementById('login-password').value;
 
